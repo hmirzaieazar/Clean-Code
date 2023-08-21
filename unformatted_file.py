5f0ceabb1f3315a0112116ca7d68c6e9e567368d
Hedieh Mirzaieazar <Hedieh.mirzaieazar@rwth-aachen.de>
2023-08-21 06:47:54 +0200

    adding unformatted file to investigate lintering tools

---
+++ unformatted_file.py
@@ -0,0 +1,25 @@
+# pylint: disable=missing-module-docstring
+import numpy as np
+
+
+def addition(
+    number_a: int,
+    number_b: int,
+):
+    return number_a + number_b
+
+
+print(
+    f"Here is the result of summation of two big number 10000000 + 2774895994 = {sum(10000000,2774895994)}"
+)
+hed = np.array([1, 2, 3])
+RESULT = (
+    10000000
+    + 2000000
+    + 4000000
+    + 1000000
+    + 10000000
+    + 2000000
+    + 4000000
+    + 1000000
+)
